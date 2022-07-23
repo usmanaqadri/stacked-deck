f8b1b87222c68b4217da78cb712d1f4a77316c29
usmanaqadri <usmanaqadri@gmail.com>
2022-07-23 08:59:35 -0400

    styling, animations, backgrounds, music

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-// global game state object that will be updated throughout the game. Right now initialized with default values
-
 function startGame() {
+  // When we start the game, we want to create our global game state object that will be updated throughout the game. Right now initialized with default values
   window.gameState = {
     players: [
       {
@@ -31,19 +30,18 @@ function startGame() {
     stackedDeck: new Array(16).fill({
       id: null,
       isBad: false,
-      isFlipped: false,
     }),
     currentPlayer: null,
   };
+  document.getElementById("root").classList.remove("gameboard");
   characterSelect();
 }
 
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-  startGame();
-});
 function characterSelect() {
   document.getElementById("root").innerHTML = `
-  <h1 class="red">Choose your player!</h1>
+  <audio src="../assets/music/character0.mp3" id="character-music"></audio>
+  <audio loop src="../assets/music/character1.mp3" id="character-music1"></audio>
+  <h1 class="character-select-header">Choose your player!</h1>
     <h2 id="current-select">Player 1 select:</h2>
     <div class="character-grid">
         <div class="box">Mario</div>
@@ -56,34 +54,71 @@ function characterSelect() {
         <div class="box">Samus</div>
     </div>
     <div class="player-grid">
-        <div class="box2">Player 1: </div>
-        <div class="box2">Player 2: </div>
-        <div class="box2">Player 3: </div>
-        <div class="box2">Player 4: </div>
+        <div class="box2">
+          <p contentEditable="true">Player 1:</p>
+          <img class="blackbox" height = "100px" src="../assets/blackbox.jpg"/>
+        </div>
+        <div class="box2">
+          <p contentEditable="true">Player 2:</p> 
+          <img class="blackbox" height = "100px" src="../assets/blackbox.jpg"/>
+        </div>
+        <div class="box2">
+          <p contentEditable="true">Player 3:</p>
+          <img class="blackbox" height = "100px" src="../assets/blackbox.jpg"/>
+        </div>
+        <div class="box2">
+          <p contentEditable="true">Player 4:</p>
+          <img class="blackbox" height = "100px" src="../assets/blackbox.jpg"/>
+        </div>
     </div>
   `;
-
+  document.getElementById("character-music").play();
+  document.getElementById("character-music").onended = function () {
+    document.getElementById("character-music1").play();
+  };
+  // this is how we add click events when we select the characters
   const characterDivs = document
     .querySelector(".character-grid")
     .querySelectorAll("div");
+  characterDivs.forEach((div, index) => {
+    div.setAttribute("data-key", `${index}`);
+    div.addEventListener("mouseover", handleHover);
+    div.addEventListener("mouseout", handleLeave);
+    div.addEventListener("click", handleClick);
+  });
 
-  characterDivs.forEach((div) => div.addEventListener("click", handleClick));
-
-  // this counter is how we're going to update Player 1-4s character selection
+  // this counter is how we're going to update Player 1-4s character selection when we click
   let counter = 1;
-  function handleEnter(e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      playGame();
-    }
+  function handleHover(e) {
+    console.log(e.target);
+    const playerDivs = document
+      .querySelector(".player-grid")
+      .querySelectorAll("div");
+    playerDivs[counter - 1].querySelector(
+      "img"
+    ).src = `../assets/characters/${e.target.innerText.toLowerCase()}.webp`;
+    playerDivs[counter - 1].querySelector("img").classList.remove("blackbox");
+    playerDivs[counter - 1].querySelector("img").classList.add("move");
+  }
+  function handleLeave() {
+    const currBox = document
+      .querySelector(".player-grid")
+      .querySelectorAll("div")
+      [counter - 1].querySelector("img");
+    currBox.src = "../assets/blackbox.jpg";
+    currBox.classList.add("blackbox");
   }
   function handleClick(e) {
+    // handleLeave();
     if (counter <= 4) {
       document.getElementById("current-select").textContent = `Player ${
         counter + 1
       } select:`;
       if (counter === 4) {
-        document.addEventListener("keyup", handleEnter);
+        // adding event listener that can only be clicked once
+        // https://stackoverflow.com/questions/3723914/remove-eventlistener-in-javascript-after-event-occurred
+        document.addEventListener("keyup", handleEnter, { once: true });
+
         document.getElementById("current-select").innerHTML = `
           <h2>Press Enter to start playing!</h2>
         `;
@@ -96,17 +131,22 @@ function characterSelect() {
       const playerDivs = document
         .querySelector(".player-grid")
         .querySelectorAll("div");
-      playerDivs[counter - 1].innerHTML += "<br/>" + e.target.innerHTML;
+      playerDivs[counter - 1].querySelector(
+        "img"
+      ).src = `../assets/characters/${e.target.innerText.toLowerCase()}.webp`;
+      playerDivs[counter - 1]
+        .querySelector("img")
+        .classList.remove("move", "blackbox");
       counter++;
     } else {
-      console.log("e.target.innerHTML");
+      console.log(e.target);
     }
-
-    //   gameState.players.forEach((player) => {
-    //     if (player.id === index + 1) {
-    //       player.name = e.target.innerHTML;
-    //     }
-    //   });
+  }
+  function handleEnter(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      playGame();
+    }
   }
 
   generateCards();
@@ -117,14 +157,13 @@ function generateCards() {
   gameState.stackedDeck = new Array(16).fill({
     id: null,
     isBad: false,
-    isFlipped: false,
   });
-  const cardsToTurnBad = sixRandNums();
-  //   this code below is to assign each card object its unique ID
+  //   this code below is to assign each of the 16 cards its unique ID
   gameState.stackedDeck = gameState.stackedDeck.map((card, index) => {
     return { ...card, id: index + 1 };
   });
-  //   this code below makes the matching card object from the sixRandNums() to be BAD
+  //   this code below uses the returned array sixRandNums() to make only the cards with a corresponding ID become bad
+  const cardsToTurnBad = sixRandNums();
   gameState.stackedDeck.forEach((card) => {
     if (cardsToTurnBad.includes(card.id)) {
       gameState.stackedDeck[card.id - 1].isBad = true;
@@ -132,6 +171,7 @@ function generateCards() {
   });
 }
 
+// this function returns an array of six unique integers from 1-16
 function sixRandNums() {
   const resultArr = [];
   while (resultArr.length !== 6) {
@@ -144,32 +184,46 @@ function sixRandNums() {
 }
 
 function playGame() {
-  // let currPlayerIndex = 0;
+  // this randomizes which player goes first
   gameState.currentPlayer = gameState.players[Math.floor(Math.random() * 4)];
+
+  // this renders our root with the gameboard
   document.getElementById("root").innerHTML = `
-    <h1>Player ${gameState.currentPlayer.id}, pick a card!</h1>
-    <div class="game-container">
-      <div id="game">
-      </div>
-    </div>
+  <audio src="../assets/music/stage0.mp3" id="stage-music"></audio>
+  <audio loop src="../assets/music/stage1.mp3" id="stage-music1"></audio>
+  <h1>Player ${gameState.currentPlayer.id}, pick a card!</h1>
+  <div class="game-container">
+  <div id="game">
+  </div>
+  </div>
   `;
-  console.log(gameState);
+  document.getElementById("stage-music").play();
+  document.getElementById("stage-music").onended = function () {
+    document.getElementById("stage-music1").play();
+  };
+  document.getElementById("root").classList.add("gameboard");
+  // so we don't have to see 16 lines of the same thing
   const cardDivs = new Array(16).fill(`
-    <div class="game-card"></div>
+  <div class="game-card"></div>
   `);
   cardDivs.forEach(
     (card) => (document.getElementById("game").innerHTML += card)
   );
+  // this gives each rendered card div its unique ID
+  document.querySelectorAll(".game-card").forEach((element, index) => {
+    element.setAttribute("id", `card${index + 1}`);
+    element.addEventListener("click", handleCardClick, { once: true });
+  });
+
   function handleCardClick(e) {
+    // this strips the clicked cards ID to just the number
     const cardId = e.target.getAttribute("id").substring(4);
-    // console.log(gameState.currentPlayer);
-    // console.log(cardId);
+
     gameState.stackedDeck.forEach((card) => {
       if (card.id == cardId) {
-        card.isFlipped = true;
         if (card.isBad) {
           alert(
-            `Player ${gameState.currentPlayer.id} has flipped a bad card and is out`
+            `Player ${gameState.currentPlayer.id} has flipped a bad card and has been eliminated.`
           );
           gameState.currentPlayer.isAlive = false;
           e.target.setAttribute("class", "bad");
@@ -178,7 +232,9 @@ function playGame() {
         }
       }
     });
+    // this is to set currentPlayer to the next person in the sequence.
     if (
+      // we want to use indexOf and length because this array may change if a player is eliminated
       gameState.players.indexOf(gameState.currentPlayer) ===
       gameState.players.length - 1
     ) {
@@ -190,22 +246,29 @@ function playGame() {
         ];
     }
 
-    document.getElementById(
-      "root"
-    ).firstElementChild.textContent = `Player ${gameState.currentPlayer.id}, pick a card!`;
+    document
+      .getElementById("root")
+      .querySelector(
+        "h1"
+      ).textContent = `Player ${gameState.currentPlayer.id}, pick a card!`;
+
+    // this is to update our gameState object to have only players still alive
     gameState.players = gameState.players.filter(
       (player) => player.isAlive === true
     );
-    console.log(gameState);
+
+    // when only one person left, they have won the game
     if (gameState.players.length === 1) {
-      alert(`Player ${gameState.currentPlayer.id} has won!`);
-      document.getElementById("root").innerHTML = `
+      document
+        .getElementById("root")
+        .querySelector(
+          "h1"
+        ).textContent = `Player ${gameState.currentPlayer.id} has won!`;
+
+      // removed board and give option to play again
+      document.getElementById("root").lastElementChild.innerHTML = `
       <button onclick='startGame()'>Play again</button>
       `;
     }
   }
-  document.querySelectorAll(".game-card").forEach((element, index) => {
-    element.setAttribute("id", `card${index + 1}`);
-    element.addEventListener("click", handleCardClick);
-  });
 }
